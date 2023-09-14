595d6cce474f6253fc59fc7bf6d67792b5753d85
moosa <moosaabdullahi45@gmail.com>
2023-09-14 16:50:43 +0200

    new

--- app/models.py
+++ app/models.py
@@ -44,6 +44,7 @@ class CustomUser(AbstractBaseUser):
     address = models.CharField(max_length=255)  # Business address
     type = models.CharField(max_length=20, choices=TYPE_CHOICES, default="user")
     is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=True)
     can_login_web = models.BooleanField(default=False)
 
     objects = CustomUserManager()
